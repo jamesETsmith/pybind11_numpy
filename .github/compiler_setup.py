950400eea74f6dc12f0b2545b0003f9b9b660e6c
jamesETsmith <james.smith9113@gmail.com>
2024-06-05 21:30:24 -0400

    More CI tweaks

--- .github/compiler_setup.py
+++ .github/compiler_setup.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     compiler = sys.argv[1]
     os.system("sudo apt-get update")
 
-    if "g++" in compiler:
+    if "g++" in compiler and "clang" not in compiler:
         version = compiler.split("-")[1]
         os.system(f"sudo apt-get -y install gcc-{version} g++-{version}")
     elif "clang" in compiler:
         version = compiler.split("-")[1]
+        print(f"Installing clang {compiler}")
         os.system("wget https://apt.llvm.org/llvm.sh")
         os.system("chmod +x llvm.sh")
         os.system(f"sudo ./llvm.sh {version}")
